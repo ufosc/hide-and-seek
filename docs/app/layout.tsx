63fc927566c1bbe301fbd5ed544e61497ae96c6f
Dylan <dylan.coben@gmail.com>
2025-02-06 09:57:39 -0500

    Removed header from docs

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,5 +1,5 @@
 import { Footer, Layout, Navbar } from "nextra-theme-docs";
-import { Banner, Head } from "nextra/components";
+import { Head } from "nextra/components";
 import { getPageMap } from "nextra/page-map";
 import "nextra-theme-docs/style.css";
 import React from "react";
