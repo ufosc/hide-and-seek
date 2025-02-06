9fc84a6baa7d19b10d5708b31e3db09d2e62cdc3
Dylan <dylan.coben@gmail.com>
2025-02-06 16:40:19 -0500

    Removed unused config

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -5,7 +5,6 @@ import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
-import { router } from "expo-router";
 
 export default function HomeScreen() {
   return (
