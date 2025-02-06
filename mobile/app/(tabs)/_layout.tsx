c13a2d6f36317727886e3d0bee34d2442d137b76
Rohit Dasgupta <rdg922@gmail.com>
2025-02-06 15:59:50 -0500

    Setup Auth and Signin

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,48 +1,28 @@
 import { Tabs } from "expo-router";
-import React from "react";
-import { Platform } from "react-native";
-
-import { HapticTab } from "@/components/HapticTab";
-import { IconSymbol } from "@/components/ui/IconSymbol";
-import TabBarBackground from "@/components/ui/TabBarBackground";
-import { Colors } from "@/constants/Colors";
-import { useColorScheme } from "@/hooks/useColorScheme";
+import { useAuthStore } from "@/context/AuthContext";
+import { Redirect } from "expo-router";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  const session = useAuthStore((state) => state.session);
+
+  if (!session) {
+    return <Redirect href="/login" />;
+  }
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: "absolute",
-          },
-          default: {},
-        }),
-      }}
-    >
+    <Tabs>
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
-          ),
+          headerShown: false,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="profile"
         options={{
-          title: "Explore",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
-          ),
+          title: "Profile",
+          headerShown: false,
         }}
       />
     </Tabs>
---
+++ mobile/context/AuthContext.ts
@@ -0,0 +1,38 @@
+import { create } from "zustand";
+import { Session } from "@supabase/supabase-js";
+import { supabase } from "@/lib/supabase";
+import { persist, createJSONStorage } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+interface AuthState {
+  session: Session | null;
+  user: any | null;
+  isLoading: boolean;
+  setUser: (session: Session | null) => void;
+  signOut: () => Promise<void>;
+}
+
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set, get) => ({
+      session: null,
+      user: null,
+      isLoading: false,
+      setUser: (session: Session | null) => {
+        set({ session: session, user: session?.user || null });
+      },
+      signOut: async () => {
+        const { error } = await supabase.auth.signOut();
+        if (error) {
+          console.log("Error signing out:", error);
+        } else {
+          set({ session: null, user: null });
+        }
+      },
+    }),
+    {
+      name: "auth-storage",
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+);
