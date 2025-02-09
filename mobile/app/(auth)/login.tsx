66773ac42a8744f5d96b8c2334c8b3abb1310ca0
santiago emanuel mustafa font <santiagomustafa1145@gmail.com>
2025-02-09 14:25:20 -0300

    feature: Implemented Sign In Page UI Based on Figma Design

--- mobile/app/(auth)/login.tsx
+++ mobile/app/(auth)/login.tsx
@@ -1,8 +1,16 @@
 import React, { useState } from "react";
-import { Alert, StyleSheet, View } from "react-native";
+import {
+  Alert,
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { supabase } from "@/lib/supabase";
-import { Button, Input } from "@rneui/themed";
 import { useRouter } from "expo-router";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function Auth() {
   const [email, setEmail] = useState("");
@@ -23,73 +31,162 @@ export default function Auth() {
     router.push("/(tabs)");
   }
 
-  async function signUpWithEmail() {
-    setLoading(true);
-    const { error } = await supabase.auth.signUp({
-      email: email,
-      password: password,
-    });
-
-    if (error) Alert.alert(error.message);
-    else {
-      Alert.alert("Check your email for verification!");
-      router.push("/(tabs)");
-    }
-    setLoading(false);
-  }
-
   return (
     <View style={styles.container}>
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Input
-          label="Email"
-          leftIcon={{ type: "font-awesome", name: "envelope" }}
-          onChangeText={(text) => setEmail(text)}
-          value={email}
-          placeholder="email@address.com"
-          autoCapitalize={"none"}
-        />
+      <View style={styles.containerForm}>
+        <Text style={styles.title}>Welcome Back!</Text>
+
+        <View style={styles.form}>
+          <View>
+            <TextInput
+              style={styles.input}
+              placeholder="Email"
+              placeholderTextColor="#A3A3A3"
+              keyboardType="email-address"
+              onChangeText={setEmail}
+              value={email}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              placeholderTextColor="#A3A3A3"
+              secureTextEntry
+              onChangeText={setPassword}
+              value={password}
+            />
+          </View>
+          <Text style={styles.forgotPassword}>Forgot your password?</Text>
+
+          <TouchableOpacity
+            style={styles.loginButton}
+            onPress={signInWithEmail}
+            disabled={loading}
+          >
+            <Text style={styles.loginText}>Log in</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.dividerContainer}>
+          <View style={styles.divider} />
+          <Text style={styles.dividerText}>or continue with</Text>
+          <View style={styles.divider} />
+        </View>
+
+        <View style={styles.socialButtonContainer}>
+          <TouchableOpacity style={styles.socialButton} disabled={loading}>
+            <Image
+              source={require("@/assets/images/google-icon.png")}
+              style={{ width: 24, height: 24 }}
+            />
+            <Text style={styles.socialText}>Continue with Google</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.socialButton} disabled={loading}>
+            <AntDesign name="apple1" size={24} color="white" />
+            <Text style={styles.socialText}>Continue with Apple</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      <View style={styles.verticallySpaced}>
-        <Input
-          label="Password"
-          leftIcon={{ type: "font-awesome", name: "lock" }}
-          onChangeText={(text) => setPassword(text)}
-          value={password}
-          secureTextEntry={true}
-          placeholder="Password"
-          autoCapitalize={"none"}
-        />
-      </View>
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Button
-          title="Sign in"
-          disabled={loading}
-          onPress={() => signInWithEmail()}
-        />
-      </View>
-      <View style={styles.verticallySpaced}>
-        <Button
-          title="Sign up"
-          disabled={loading}
-          onPress={() => signUpWithEmail()}
-        />
-      </View>
+
+      <Text style={styles.signUpText}>
+        Don’t have an account? <Text style={styles.signUpLink}>Sign up</Text>
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 40,
-    padding: 12,
+    flex: 1,
+    backgroundColor: "#1A1525",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    paddingBottom: 40,
+    paddingTop: 80,
   },
-  verticallySpaced: {
-    paddingTop: 4,
-    paddingBottom: 4,
-    alignSelf: "stretch",
+  containerForm: {
+    maxWidth: 450,
+    width: "100%",
+    marginHorizontal: "auto",
+    gap: 16,
   },
-  mt20: {
+  form: {},
+  title: {
+    color: "white",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 50,
+    textAlign: "center",
+  },
+  input: {
+    width: "100%",
+    backgroundColor: "#2A213D",
+    color: "white",
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  forgotPassword: {
+    color: "#A3A3A3",
+    fontSize: 14,
+    alignSelf: "flex-end",
+    marginBottom: 20,
+  },
+  loginButton: {
+    width: "100%",
+    backgroundColor: "white",
+    paddingVertical: 15,
+    borderRadius: 10,
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  loginText: {
+    color: "black",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  dividerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    marginBottom: 20,
+  },
+  divider: {
+    flex: 1,
+    height: 1,
+    backgroundColor: "#fff",
+  },
+  dividerText: {
+    color: "#A3A3A3",
+    marginHorizontal: 10,
+  },
+  socialButtonContainer: {
+    gap: 12,
+  },
+  socialButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "#4A4A4A",
+    paddingVertical: 15,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  socialText: {
+    color: "white",
+    marginLeft: 10,
+    fontSize: 16,
+  },
+  signUpText: {
+    color: "#A3A3A3",
     marginTop: 20,
+    textAlign: "center",
+  },
+  signUpLink: {
+    color: "white",
+    fontWeight: "bold",
+    cursor: "pointer",
   },
 });
