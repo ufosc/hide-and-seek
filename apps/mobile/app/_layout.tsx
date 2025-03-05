f2b946c9497c6a903b0e1bd4b03ce7acb85c1897
Dylan Coben <59846502+dyland88@users.noreply.github.com>
2025-03-05 17:17:10 -0500

    Added typescript and rnr config

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -2,6 +2,7 @@ import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
+  Theme,
 } from "@react-navigation/native";
 
 import {
@@ -9,6 +10,7 @@ import {
   QueryClientProvider,
   useQuery,
 } from "@tanstack/react-query";
+import "~/global.css";
 
 const queryClient = new QueryClient();
 
@@ -17,18 +19,45 @@ import * as SplashScreen from "expo-splash-screen";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { useColorScheme } from "react-native";
 import { supabase } from "@/lib/supabase";
 import { useAuthStore } from "@/store/authStore";
+import { NAV_THEME } from "~/lib/constants";
+import { useColorScheme } from "~/lib/useColorScheme";
+import React from "react";
 
 SplashScreen.preventAutoHideAsync();
 
+const LIGHT_THEME: Theme = {
+  ...DefaultTheme,
+  colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+  ...DarkTheme,
+  colors: NAV_THEME.dark,
+};
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from "expo-router";
+
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
   const setUser = useAuthStore((state) => state.setUser);
+
+  const hasMounted = React.useRef(false);
+  const { colorScheme, isDarkColorScheme } = useColorScheme();
+  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
+
+  React.useLayoutEffect(() => {
+    if (hasMounted.current) {
+      return;
+    }
+    setIsColorSchemeLoaded(true);
+    hasMounted.current = true;
+  }, []);
 
   useEffect(() => {
     async function loadSession() {
@@ -58,7 +87,7 @@ export default function RootLayout() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="(auth)" options={{ headerShown: false }} />
